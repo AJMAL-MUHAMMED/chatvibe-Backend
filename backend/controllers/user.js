8b68bba1f5c39769c566415edf37459cc3ae4249
Muhammed Ajmal <ajmalktl313@gmail.com>
2022-12-14 17:23:59 +0530

    completed friend request page

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -11,6 +11,7 @@ const bcrypt = require("bcrypt");
 const { generateToken } = require("../helpers/tokens");
 const { sendVerificationEmail, sendResetCode } = require("../helpers/mailer");
 const generateCode = require("../helpers/generateCode");
+const { Mongoose } = require("mongoose");
 exports.register = async (req, res) => {
   try {
     const {
@@ -568,6 +569,25 @@ exports.removeFromSearch = async (req, res) => {
       },
       { $pull: { search: { user: searchUser } } }
     );
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getFriendsPageInfos = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id)
+      .select("friends requests")
+      .populate("friends", "first_name last_name picture username")
+      .populate("requests", "first_name last_name picture username");
+    const sentRequests = await User.find({
+      requests: req.user.id
+    }).select("first_name last_name picture username");
+    res.json({
+      friends: user.friends,
+      requests: user.requests,
+      sentRequests,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
